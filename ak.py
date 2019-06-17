3a42dc38b182b0c40fd997f4639864a0d4f46cf1
ekshu singh <singhekshu@gmail.com>
2019-06-17 15:35:41 +0530

    python uploaded

---
+++ ak.py
@@ -0,0 +1,14 @@
+mydict = {
+    "carl": "40",
+    "alan": "2",
+    "bob": "1",
+    "danny": "3",
+}
+
+for key in mydict.keys():
+	mydict[key]=int(mydict[key])
+
+
+
+for key, value in sorted(mydict.items(), key=lambda item: item[1]):
+    print("%s: %s" % (key, value))
